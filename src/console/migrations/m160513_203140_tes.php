c47e4aba4ceeb2332e2f932d20d7b504d597b99b
iqueak <zyb@lds.net.ua>
2016-05-14 15:08:26 +0300

    tes migration + оптимизация полей,связи,droptable

--- src/console/migrations/m160513_203140_tes.php
+++ src/console/migrations/m160513_203140_tes.php
@@ -8,8 +8,19 @@ class m160513_203140_tes extends Migration
     {
         $this->createTable('{{%bafs}}', [
             'player_id' => $this->primaryKey(),
-            'baf_name' => $this->string()->notNull(),
-            'baf_desc' => $this->text()
+            'name' => $this->string()->notNull(),
+            'desc' => $this->text()
+        ]);
+        $this->createTable('{{%perks}}', [
+            'player_id' => $this->primaryKey(),
+            'name' => $this->string()->notNull(),
+            'value' => $this->integer()->notNull(),
+            'desc' => $this->text()
+        ]);
+        $this->createTable('{{%spells}}', [
+            'player_id' => $this->primaryKey(),
+            'name' => $this->string()->notNull(),
+            'desc' => $this->text()
         ]);
         $this->createTable('{{%inventory}}', [
             'inventory_id' => $this->primaryKey(),
@@ -28,13 +39,7 @@ class m160513_203140_tes extends Migration
             'cost' => $this->integer()->notNull(),
             'mbaff_first' => $this->string()->notNull(),
             'mbaff_second' => $this->string()->notNull(),
-            'item_desc' => $this->text()->notNull()
-        ]);
-        $this->createTable('{{%perks}}', [
-            'player_id' => $this->primaryKey(),
-            'perk_name' => $this->string()->notNull(),
-            'perk_value' => $this->integer()->notNull(),
-            'perk_desc' => $this->text()
+            'desc' => $this->text()->notNull()
         ]);
         $this->createTable('{{%players}}', [
             'player_id' => $this->primaryKey(),
@@ -56,11 +61,6 @@ class m160513_203140_tes extends Migration
             'good_lucky' => $this->integer()->notNull(),
             'shout' => $this->integer()->notNull()
         ]);
-        $this->createTable('{{%spells}}', [
-            'player_id' => $this->primaryKey(),
-            'spell_name' => $this->string()->notNull(),
-            'spell_desc' => $this->text()
-        ]);
         $this->createTable('{{%skills}}', [
             'player_id' => $this->primaryKey(),
             'illusion' => $this->integer()->notNull(),
@@ -77,17 +77,36 @@ class m160513_203140_tes extends Migration
             'archery' => $this->integer()->notNull(),
             'light_armor' => $this->integer()->notNull(),
             'sneak' => $this->integer()->notNull(),
-            'lockpicking' => $this->integer()->notNull(),
+            'lock_picking' => $this->integer()->notNull(),
             'pickpocket' => $this->integer()->notNull(),
             'speech' => $this->integer()->notNull(),
             'alchemy' => $this->integer()->notNull()
         ]);
+        $this->addForeignKey('fk_bafs_player_id', '{{%bafs}}', 'player_id', '{{%players}}', 'player_id', null, 'CASCADE');
+        $this->addForeignKey('fk_perks_player_id', '{{%perks}}', 'player_id', '{{%players}}', 'player_id', null, 'CASCADE');
+        $this->addForeignKey('fk_spells_player_id', '{{%spells}}', 'player_id', '{{%players}}', 'player_id', null, 'CASCADE');
+        $this->addForeignKey('fk_inventory_player_id', '{{%inventory}}', 'player_id', '{{%players}}', 'player_id', null, 'CASCADE');
+        $this->addForeignKey('fk_inventory_item_id', '{{%inventory}}', 'item_id', '{{%item_list}}', 'item_id', null, 'CASCADE');
+        $this->addForeignKey('fk_stats_player_id', '{{%stats}}', 'player_id', '{{%players}}', 'player_id', null, 'CASCADE');
+        $this->addForeignKey('fk_skills_player_id', '{{%skills}}', 'player_id', '{{%players}}', 'player_id', null, 'CASCADE');
     }
 
     public function down()
     {
-        echo "m160513_203140_tes cannot be reverted.\n";
-
-        return false;
+        $this->dropForeignKey('fk_bafs_player_id', '{{%bafs}}');
+        $this->dropForeignKey('fk_perks_player_id', '{{%perks}}');
+        $this->dropForeignKey('fk_spells_player_id', '{{%spells}}');
+        $this->dropForeignKey('fk_inventory_player_id', '{{%inventory}}');
+        $this->dropForeignKey('fk_inventory_item_id', '{{%inventory}');
+        $this->dropForeignKey('fk_stats_player_id', '{{%stats}}');
+        $this->dropForeignKey('fk_skills_player_id', '{{%skills}}');
+        $this->dropTable('{{%bafs}}');
+        $this->dropTable('{{%perks}}');
+        $this->dropTable('{{%spells}}');
+        $this->dropTable('{{%inventory}}');
+        $this->dropTable('{{%item_list}}');
+        $this->dropTable('{{%players}}');
+        $this->dropTable('{{%stats}}');
+        $this->dropTable('{{%skills}}');
     }
 }
